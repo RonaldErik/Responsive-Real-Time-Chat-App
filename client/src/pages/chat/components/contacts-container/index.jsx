5011350d1bd042ae01e3f51419bca588387af079
TLiiv <110547191+TLiiv@users.noreply.github.com>
2025-02-24 17:24:21 +0200

    empty chat container with animation

---
+++ client/src/pages/chat/components/contacts-container/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Lottie from 'react-lottie';
+import { animationDefaultOptions } from '@/lib/utils';
+
+const ContactsContainer = () => {
+  return (
+    <div className='relative md:w-[35vw] lg:w-[30vw] xl:w-[20vw] bg-[#1b1c24] border-r-2 border-[#2f303b] w-full'>
+      contacts
+   </div>
+  )
+}
+
+export default ContactsContainer
